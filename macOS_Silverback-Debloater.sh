8bd405e6a7493695b7e7e29c4aee88aaf36dbb98
realies <5107843+realies@users.noreply.github.com>
2024-12-26 20:41:30 +0000

    add more services, improve logs

--- macOS_Silverback-Debloater.sh
+++ macOS_Silverback-Debloater.sh
@@ -1,245 +1,314 @@
 #!/bin/bash
 
 # macOS Silverback Debloater
-# v1.0 by Wamphyre
+# A privacy-focused system debloater for macOS
+# Requirements: SIP disabled ("csrutil disable" from Recovery)
+#
+# Note: Modifications are written to:
+# /private/var/db/com.apple.xpc.launchd/disabled.plist
+# /private/var/db/com.apple.xpc.launchd/disabled.501.plist
+#
+# To revert all changes:
+# 1. sudo rm -r /private/var/db/com.apple.xpc.launchd/*
+# 2. Reboot your system
 
-# Disabling SIP is required  ("csrutil disable" from Terminal in Recovery)
-# Modifications are written in /private/var/db/com.apple.xpc.launchd/ disabled.plist, disabled.501.plist
-# To revert, delete /private/var/db/com.apple.xpc.launchd/ disabled.plist and disabled.501.plist and reboot; sudo rm -r /private/var/db/com.apple.xpc.launchd/*
+# Print with color
+print_status() {
+    local color=$1
+    local message=$2
+    case $color in
+        "green") echo -e "\033[32m$message\033[0m" ;;
+        "red") echo -e "\033[31m$message\033[0m" ;;
+        *) echo "$message" ;;
+    esac
+}
 
-# Disabling Spotlight
-echo "Disabling Spotlight"
-sudo mdutil -i off -a
-echo "Done"
+# Check if SIP is disabled
+check_sip() {
+    if [[ $(csrutil status) == *"enabled"* ]]; then
+        print_status "red" "Error: System Integrity Protection is enabled. Please disable it first."
+        print_status "red" "1. Restart your Mac and hold Command+R during startup"
+        print_status "red" "2. Open Terminal from Utilities menu"
+        print_status "red" "3. Run: csrutil disable"
+        print_status "red" "4. Restart and run this script again"
+        exit 1
+    fi
+}
 
-# Disable animations, and heavy desktop effects
-echo "Disabing animations and desktop effects"
-sudo defaults write /Library/Preferences/com.apple.loginwindow DesktopPicture ""
-defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
-defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
-defaults write -g QLPanelAnimationDuration -float 0
-defaults write com.apple.dock autohide-time-modifier -float 0
-defaults write com.apple.Dock autohide-delay -float 0
-defaults write com.apple.dock expose-animation-duration -float 0.001
-defaults write com.apple.dock launchanim -bool false
-defaults write com.apple.finder DisableAllAnimations -bool true
-defaults write com.apple.Accessibility DifferentiateWithoutColor -int 1
-defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
-defaults write com.apple.universalaccess reduceMotion -int 1
-defaults write com.apple.universalaccess reduceTransparency -int 1
-defaults write com.apple.Accessibility ReduceMotionEnabled -int 1
-echo "Done"
+# Configure system preferences
+configure_system() {
+    print_status "green" "Configuring system preferences..."
+    
+    # Disable Spotlight indexing
+    sudo mdutil -i off -a
+    
+    # Optimize UI performance
+    defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+    defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+    defaults write -g QLPanelAnimationDuration -float 0
+    defaults write com.apple.dock autohide-time-modifier -float 0
+    defaults write com.apple.Dock autohide-delay -float 0
+    defaults write com.apple.dock expose-animation-duration -float 0.001
+    defaults write com.apple.dock launchanim -bool false
+    defaults write com.apple.finder DisableAllAnimations -bool true
+    
+    # Configure OpenDirectory
+    sudo defaults write /Library/Preferences/com.apple.opendirectoryd.plist "Delay before configured automatic" -int 0
+    sudo defaults write /Library/Preferences/com.apple.opendirectoryd.plist "Delay before automatic" -int 0
+    sudo defaults write /Library/Preferences/com.apple.opendirectoryd.plist "Module automatic" -bool false
+    sudo defaults write /Library/Preferences/com.apple.opendirectoryd.plist "Search node automatic" -bool false
 
-# Disable useless Apple services
-echo "Disabling useless and slow Apple services"
-sudo launchctl remove com.apple.CallHistoryPluginHelper
-sudo launchctl remove com.apple.AddressBook.abd
-sudo launchctl remove com.apple.ap.adprivacyd
-sudo launchctl remove com.apple.ReportPanic
-sudo launchctl remove com.apple.ReportCrash
-sudo launchctl remove com.apple.ReportCrash.Self
-sudo launchctl remove com.apple.DiagnosticReportCleanup.plist
-sudo launchctl remove com.apple.ap.adprivacyd
-sudo launchctl remove com.apple.siriknowledged
-sudo launchctl remove com.apple.helpd
-sudo launchctl remove com.apple.mobiledeviceupdater
-sudo launchctl remove com.apple.screensharing.MessagesAgent
-sudo launchctl remove com.apple.TrustEvaluationAgent
-sudo launchctl remove com.apple.iTunesHelper.launcher
-sudo launchctl remove com.apple.softwareupdate_notify_agent
-sudo launchctl remove com.apple.appstoreagent
-sudo launchctl remove com.apple.familycircled
-echo "Done"
+    # Disable application state on shutdown
+    defaults write com.apple.loginwindow TALLogoutSavesState -bool false
 
-# Disable and remove Safari services
-echo "Disabling and removing Safari services"
-sudo launchctl remove com.apple.SafariCloudHistoryPushAgent
-sudo launchctl remove com.apple.Safari.SafeBrowsing.Service
-sudo launchctl remove com.apple.SafariNotificationAgent
-sudo launchctl remove com.apple.SafariPlugInUpdateNotifier
-sudo launchctl remove com.apple.SafariHistoryServiceAgent
-sudo launchctl remove com.apple.SafariLaunchAgent
-sudo launchctl remove com.apple.SafariPlugInUpdateNotifier
-sudo launchctl remove com.apple.safaridavclient
-echo "Done"
+    print_status "green" "Disabling automatic updates..."
+    # Disable automatic updates
+    local updates=("AutomaticDownload" "AutomaticCheckEnabled" "CriticalUpdateInstall" "ConfigDataInstall")
+    for val in "${updates[@]}"; do
+        sudo defaults write com.apple.SoftwareUpdate $val -int 0
+    done
+}
 
-# Disable application state on shutdown
-echo "Disabling application state on shutdown"
-defaults write com.apple.loginwindow TALLogoutSavesState -bool false
-echo "Done"
+# Configure background services
+configure_background_services() {
+    print_status "green" "Configuring background services..."
+    
+    # Enable essential background management
+    sudo launchctl enable system/com.apple.backgroundtaskmanagement.bgservicesd
+    launchctl enable gui/501/com.apple.backgroundtaskmanagement.agent
+    
+    # Disable telemetry
+    sudo defaults write /Library/Preferences/com.apple.backgroundtaskmanagement.plist "Application Resource Usage Monitoring" -bool false
+    sudo defaults write /Library/Preferences/com.apple.backgroundtaskmanagement.plist "Data Collection" -bool false
+}
 
-# Disable facial recognition in media, telemetry, location services, java installation on demand, telnet, ftp, and netbios.
-echo "Disabling facial recognition, telemetry, location services and other invasive and insecure stuff"
-DAE="com.apple.telnetd com.apple.tftpd com.apple.ftp-proxy com.apple.analyticsd com.apple.amp.mediasharingd com.apple.mediaanalysisd com.apple.mediaremoteagent com.apple.photoanalysisd com.apple.java.InstallOnDemand com.apple.voicememod com.apple.geod com.apple.locate com.apple.locationd com.apple.netbiosd com.apple.recentsd com.apple.suggestd com.apple.spindump com.apple.metadata.mds.spindump com.apple.ReportPanic com.apple.ReportCrash com.apple.ReportCrash.Self com.apple.DiagnosticReportCleanup"
-for val in $DAE; do
-  sudo launchctl disable system/$val
-done
-echo "Done"
+# Define services to disable
+declare -a USER_SERVICES=(
+    # Analytics and Telemetry
+    'com.apple.analyticsd'
+    'com.apple.proactived'
+    'com.apple.geoanalyticsd'
+    'com.apple.diagnostics_agent'
+    'com.apple.spindump_agent'
+    'com.apple.ReportCrash'
+    'com.apple.webprivacyd'
+    'com.apple.privacyd'
+    'com.apple.osanalytics.osanalyticshelper'
+    'com.apple.UsageTrackingAgent'
+    
+    # Cloud Services
+    'com.apple.cloudd'
+    'com.apple.cloudpaird'
+    'com.apple.cloudphotod'
+    'com.apple.icloud.*'
+    'com.apple.bird'
+    'com.apple.security.cloudkeychainproxy3'
+    
+    # Safari Services
+    'com.apple.SafariCloudHistoryPushAgent'
+    'com.apple.Safari.SafeBrowsing.Service'
+    'com.apple.SafariNotificationAgent'
+    'com.apple.SafariPlugInUpdateNotifier'
+    'com.apple.SafariHistoryServiceAgent'
+    'com.apple.SafariLaunchAgent'
+    'com.apple.safaridavclient'
+    
+    # Siri and Intelligence
+    'com.apple.Siri.agent'
+    'com.apple.siriknowledged'
+    'com.apple.assistant_service'
+    'com.apple.intelligenceplatformd'
+    'com.apple.BiomeAgent'
+    'com.apple.biomesyncd'
+    'com.apple.knowledge-agent'
+    
+    # Communication & Sharing
+    'com.apple.screensharing.agent'
+    'com.apple.screensharing.menuextra'
+    'com.apple.sharingd'
+    'com.apple.sidecar-hid-relay'
+    'com.apple.sidecar-relay'
+    'com.apple.rapportd-user'
+    
+    # Unused Apple Services
+    'com.apple.Maps.*'
+    'com.apple.facetime.*'
+    'com.apple.iMessage.*'
+    'com.apple.AirPlayXPCHelper'
+    'com.apple.AddressBook.ContactsAccountsService'
+    'com.apple.AMPArtworkAgent'
+    'com.apple.AMPDeviceDiscoveryAgent'
+    'com.apple.AMPLibraryAgent'
+    'com.apple.CalendarAgent'
+    'com.apple.calaccessd'
+    'com.apple.avconferenced'
+    'com.apple.telephonyutilities.callservicesd'
+    
+    # System Services
+    'com.apple.ap.adservicesd'
+    'com.apple.ap.promotedcontentd'
+    'com.apple.dataaccess.dataaccessd'
+    'com.apple.ensemble'
+    'com.apple.familynotificationd'
+    'com.apple.financed'
+    'com.apple.followupd'
+    'com.apple.gamed'
+    'com.apple.geodMachServiceBridge'
+    'com.apple.homed'
+    'com.apple.newsd'
+    'com.apple.passd'
+    'com.apple.photolibraryd'
+    'com.apple.progressd'
+    'com.apple.remindd'
+    'com.apple.TMHelperAgent'
+    'com.apple.TMHelperAgent.SetupOffer'
+    'com.apple.triald'
+    'com.apple.videosubscriptionsd'
+    'com.apple.WiFiVelocityAgent'
+    'com.apple.weatherd'
+    
+    # Additional Telemetry
+    'com.apple.coreduetd'
+    'com.apple.studentd'
+    'com.apple.translationd'
+    'com.apple.backgroundassets.user'
+    'com.apple.accessibility.MotionTrackingAgent'
+    'com.apple.dprivacyd'
+    'com.apple.metrickitd'
+    'com.apple.ciphermld'
+    'com.apple.intelligencecontextd'
+    'com.apple.mlhostd'
+    'com.apple.idsfoundation.IDSRemoteURLConnectionAgent'
+    'com.apple.cache_delete'
+    'com.apple.CrashReporterSupportHelper'
+    'com.apple.accessibility.heard'
+    'com.apple.accessibility.HeardAgent'
+    'com.apple.sysmond'
+)
 
-# Disable automatic updates using defaults write.
-echo "Disabling automatic updates"
-UPDATES="AutomaticDownload AutomaticCheckEnabled CriticalUpdateInstall ConfigDataInstall"
-for val in $UPDATES; do
-  sudo defaults write com.apple.SoftwareUpdate $val -int 0
-done
-echo "Done"
+declare -a SYSTEM_SERVICES=(
+    # System Analytics
+    'com.apple.analyticsd'
+    'com.apple.proactived'
+    'com.apple.diagnosticd'
+    'com.apple.spindump'
+    'com.apple.osanalytics.osanalyticshelper'
+    'com.apple.systemstats.daily'
+    'com.apple.systemstats.analysis'
+    
+    # Cloud and Network
+    'com.apple.cloudd'
+    'com.apple.cloudpaird'
+    'com.apple.networkserviceproxy'
+    'com.apple.netbiosd'
+    'com.apple.ftp-proxy'
+    'com.apple.ftpd'
+    'com.apple.bootpd'
+    'com.apple.backupd'
+    'com.apple.backupd-helper'
+    'com.apple.telnetd'
+    
+    # Media and Device Services
+    'com.apple.amp.mediasharingd'
+    'com.apple.mediaremoteagent'
+    'com.apple.java.InstallOnDemand'
+    'com.apple.voicememod'
+    
+    # Location and Recent Items
+    'com.apple.locate'
+    'com.apple.locationd'
+    'com.apple.recentsd'
+    
+    # Additional System Services
+    'com.apple.biometrickitd'
+    'com.apple.ReportCrash.Root'
+    'com.apple.awdd'
+    'com.apple.AMPDevicesAgent'
+    'com.apple.diagnosticextensionsd'
+    'com.apple.eapolcfg_auth'
+    'com.apple.avatarsd'
+    'com.apple.businessservicesd'
+    'com.apple.contextstore'
+    'com.apple.contextstored'
+    'com.apple.usernoted'
+    'com.apple.containermanagerd'
+    'com.apple.coreservices.useractivityd'
+    'com.apple.powerexperienced'
+    'com.apple.nfcd'
+    'com.apple.storekitd'
+    'com.apple.storedownloadd'
+    'com.apple.fairplayd'
+    'com.apple.appstoreagent'
+    'com.apple.CoreRoutine'
+)
 
-# user
-TODISABLE=()
+# Disable services
+disable_services() {
+    print_status "green" "Disabling unnecessary services..."
+    
+    # Disable user services
+    for service in "${USER_SERVICES[@]}"; do
+        if launchctl print gui/501/${service} &>/dev/null; then
+            launchctl bootout gui/501/${service} 2>/dev/null || true
+            launchctl disable gui/501/${service} 2>/dev/null || true
+        fi
+    done
+    
+    # Explicitly unload cloudpaird
+    launchctl unload -w /System/Library/LaunchAgents/com.apple.cloudpaird.plist 2>/dev/null || true
+    
+    # Disable system services
+    for service in "${SYSTEM_SERVICES[@]}"; do
+        if sudo launchctl print system/${service} &>/dev/null; then
+            sudo launchctl bootout system/${service} 2>/dev/null || true
+            sudo launchctl disable system/${service} 2>/dev/null || true
+        fi
+    done
+}
 
-TODISABLE+=('com.apple.accessibility.MotionTrackingAgent' \
-'com.apple.AddressBook.ContactsAccountsService' \
-'com.apple.AMPArtworkAgent' \
-'com.apple.AMPDeviceDiscoveryAgent' \
-'com.apple.AMPLibraryAgent' \
-'com.apple.ap.adprivacyd' \
-'com.apple.ap.adservicesd' \
-'com.apple.ap.promotedcontentd' \
-'com.apple.assistant_service' \
-'com.apple.assistantd' \
-'com.apple.avconferenced' \
-'com.apple.BiomeAgent' \
-'com.apple.biomesyncd' \
-'com.apple.CalendarAgent' \
-'com.apple.calaccessd' \
-'com.apple.CallHistoryPluginHelper' \
-'com.apple.cloudd' \
-'com.apple.cloudpaird' \
-'com.apple.cloudphotod' \
-'com.apple.CloudPhotosConfiguration' \
-'com.apple.CloudSettingsSyncAgent' \
-'com.apple.CommCenter-osx' \
-'com.apple.ContactsAgent' \
-'com.apple.CoreLocationAgent' \
-'com.apple.dataaccess.dataaccessd' \
-'com.apple.ensemble' \
-'com.apple.familycircled' \
-'com.apple.familycontrols.useragent' \
-'com.apple.familynotificationd' \
-'com.apple.financed' \
-'com.apple.followupd' \
-'com.apple.gamed' \
-'com.apple.geod' \
-'com.apple.geodMachServiceBridge' \
-'com.apple.homed' \
-'com.apple.icloud.fmfd' \
-'com.apple.iCloudNotificationAgent' \
-'com.apple.iCloudUserNotifications' \
-'com.apple.icloud.searchpartyuseragent' \
-'com.apple.imagent' \
-'com.apple.imautomatichistorydeletionagent' \
-'com.apple.imtransferagent' \
-'com.apple.intelligenceplatformd' \
-'com.apple.itunescloudd' \
-'com.apple.knowledge-agent' \
-'com.apple.ManagedClient.cloudconfigurationd' \
-'com.apple.ManagedClientAgent.enrollagent' \
-'com.apple.Maps.mapspushd' \
-'com.apple.Maps.pushdaemon' \
-'com.apple.mediaanalysisd' \
-'com.apple.networkserviceproxy' \
-'com.apple.networkserviceproxy-osx' \
-'com.apple.mediastream.mstreamd' \
-'com.apple.newsd' \
-'com.apple.nsurlsessiond' \
-'com.apple.parsec-fbf' \
-'com.apple.parsecd' \
-'com.apple.passd' \
-'com.apple.photoanalysisd' \
-'com.apple.photolibraryd' \
-'com.apple.progressd' \
-'com.apple.protectedcloudstorage.protectedcloudkeysyncing' \
-'com.apple.quicklook' \
-'com.apple.quicklook.ui.helper' \
-'com.apple.quicklook.ThumbnailsAgent' \
-'com.apple.rapportd-user' \
-'com.apple.remindd' \
-'com.apple.routined' \
-'com.apple.SafariCloudHistoryPushAgent' \
-'com.apple.screensharing.agent' \
-'com.apple.screensharing.menuextra' \
-'com.apple.screensharing.MessagesAgent' \
-'com.apple.ScreenTimeAgent' \
-'com.apple.security.cloudkeychainproxy3' \
-'com.apple.sharingd' \
-'com.apple.sidecar-hid-relay' \
-'com.apple.sidecar-relay' \
-'com.apple.Siri.agent' \
-'com.apple.siri.context.service' \
-'com.apple.macos.studentd' \
-'com.apple.siriknowledged' \
-'com.apple.suggestd' \
-'com.apple.tipsd' \
-'com.apple.telephonyutilities.callservicesd' \
-'com.apple.TMHelperAgent' \
-'com.apple.TMHelperAgent.SetupOffer' \
-'com.apple.triald' \
-'com.apple.universalaccessd' \
-'com.apple.UsageTrackingAgent' \
-'com.apple.videosubscriptionsd' \
-'com.apple.WiFiVelocityAgent' \
-'com.apple.weatherd')
+# Print essential services that remain enabled
+print_essential_services() {
+    print_status "green" "The following essential services remain enabled:"
+    echo "- com.apple.softwareupdated (System updates)"
+    echo "- com.apple.XprotectFramework (Security protection)"
+    echo "- com.apple.MRTd (Malware protection)"
+    echo "- com.apple.apsd (Basic push notifications)"
+    echo "- com.apple.secd (Keychain security)"
+    echo "- com.apple.identityservicesd (Basic system authentication)"
+    echo "- com.apple.audio.* (Audio functionality)"
+    echo "- com.apple.coreaudiod (Core Audio)"
+}
 
-for agent in "${TODISABLE[@]}"
-do
-	launchctl bootout gui/501/${agent}
-	launchctl disable gui/501/${agent}
-done
+# Print Spotlight alternatives
+print_spotlight_alternatives() {
+    print_status "green" "Spotlight alternatives:"
+    echo "- Raycast (privacy-focused): https://www.raycast.com/"
+    echo "- Albert (opensource): https://albertlauncher.github.io/"
+    echo "- Cerebro (opensource): https://github.com/cerebroapp/cerebro"
+    echo "- Quicksilver (opensource): https://qsapp.com/"
+}
 
-launchctl unload -w /System/Library/LaunchAgents/com.apple.cloudpaird.plist
+# Main execution
+main() {
+    print_status "green" "Starting macOS Silverback Debloater..."
+    check_sip
+    configure_system
+    configure_background_services
+    disable_services
+    print_essential_services
+    print_spotlight_alternatives
+    
+    print_status "green" "Debloat complete!"
+    read -e -p "Do you want to reboot now? (y/N) " yn
+    if [[ $yn == "y" ]]; then
+        print_status "green" "Rebooting..."
+        sleep 3
+        sudo reboot
+    else
+        print_status "green" "Please reboot your system manually to apply all changes."
+        exit 0
+    fi
+}
 
-# system
-TODISABLE=()
-
-TODISABLE+=('com.apple.bootpd' \
-'com.apple.backupd' \
-'com.apple.backupd-helper' \
-'com.apple.cloudd' \
-'com.apple.cloudpaird' \
-'com.apple.cloudphotod' \
-'com.apple.CloudPhotosConfiguration' \
-'com.apple.CoreLocationAgent' \
-'com.apple.coreduetd' \
-'com.apple.dhcp6d' \
-'com.apple.familycontrols' \
-'com.apple.findmymacmessenger' \
-'com.apple.followupd' \
-'com.apple.FollowUpUI' \
-'com.apple.ftp-proxy' \
-'com.apple.ftpd' \
-'com.apple.GameController.gamecontrollerd' \
-'com.apple.icloud.fmfd' \
-'com.apple.icloud.searchpartyd' \
-'com.apple.itunescloudd' \
-'com.apple.locationd' \
-'com.apple.ManagedClient.cloudconfigurationd' \
-'com.apple.networkserviceproxy' \
-'com.apple.netbiosd' \
-'com.apple.nsurlsessiond' \
-'com.apple.protectedcloudstorage.protectedcloudkeysyncing' \
-'com.apple.rapportd' \
-'com.apple.screensharing' \
-'com.apple.security.cloudkeychainproxy3' \
-'com.apple.siri.morphunassetsupdaterd' \
-'com.apple.siriinferenced' \
-'com.apple.triald.system' \
-'com.apple.wifianalyticsd')
-
-for daemon in "${TODISABLE[@]}"
-do
-	sudo launchctl bootout system/${daemon}
-	sudo launchctl disable system/${daemon}
-done
-
-# Reboot
-echo "Debloat done"
-read -e -p "(y/N) Do you want to reboot now? " yn
-if [ "$yn" = "y" ]; then
-  sleep 3
-  echo "Rebooting..."
-  sudo reboot
-else
-  sleep 3
-  echo "Defaulting to no."
-  exit
-fi
+# Run the script
+main
